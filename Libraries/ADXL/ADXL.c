af4298b5f7a6ab39c4079a02275655c78175d232
Ayush <kudoshin32@gmail.com>
2019-07-10 14:19:39 +0530

    library: finishing touches

--- Libraries/ADXL/ADXL.c
+++ Libraries/ADXL/ADXL.c
@@ -1,9 +1,9 @@
 #include "ADXL.h"
 
 void initADXL(){
-	pcf8591Setup();
+	pcf8591Setup(); //Initializing the ADC PCF8591
 	}
 
 int readADXL(int addr){
-	return pcf8591Read(addr);
+	return pcf8591Read(addr); //Get value from the ADC depending on the pin which was given
 	}
